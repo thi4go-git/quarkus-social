73460cf2512be5175b7e3c7c516184eeb72a60d8
Thiago Junior Amorim De Melo (GOVBR GO - DRC Central) <thiago.melo@govbr.com.br>
2022-12-28 19:52:51 -0300

    Updates

---
+++ src/main/java/br/com/social/configuration/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package br.com.social.configuration;
+
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Contact;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.info.License;
+
+import javax.ws.rs.core.Application;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "API-Social",
+                version = "1.0.0",
+                contact = @Contact(
+                        name = "Exemplo nome",
+                        url = "www.url.com.br",
+                        email = "email.com.br"),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.apache.org/licenses/LICENSE-2.0.html"
+                )
+        )
+)
+public class SwaggerConfig extends Application {
+
+}
