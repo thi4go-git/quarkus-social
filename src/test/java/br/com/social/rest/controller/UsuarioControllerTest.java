408334dbd45b970113e28801889265d885dffae4
Thiago Junior Amorim De Melo (GOVBR GO - DRC Central) <thiago.melo@govbr.com.br>
2022-12-28 19:08:07 -0300

    Adicionando Swagger

--- src/test/java/br/com/social/rest/controller/UsuarioControllerTest.java
+++ src/test/java/br/com/social/rest/controller/UsuarioControllerTest.java
@@ -1,19 +1,25 @@
 package br.com.social.rest.controller;
 
 import br.com.social.rest.dto.UsuarioDTO;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.*;
+
+
 
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
 
 @QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestHTTPEndpoint(UsuarioController.class)
 class UsuarioControllerTest {
 
     @Test
-    @DisplayName("Deve Criar usuário POST")
+    @DisplayName("Deve Criar usuário")
+    @Order(1)
     public void criarUsuarioTest() {
         var userDTO = new UsuarioDTO();
         userDTO.setIdade(15);
@@ -23,12 +29,26 @@ class UsuarioControllerTest {
                 .contentType(ContentType.JSON)
                 .body(userDTO)
                 .when()
-                .post("/usuarios")
+                .post()
                 .then()
                 .extract().response();
         //
         assertEquals(201, resposta.statusCode());
         assertNotNull(resposta.jsonPath().getString("id"));
+    }
+
+
+    @Test
+    @DisplayName("deve Listar Todos Usuarios")
+    @Order(2)
+    public void deveListarTodosUsuarios() {
+        var resposta = given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get()
+                .then()
+                .statusCode(200)
+                .body("size()", Matchers.is(1));
     }
 
 }
